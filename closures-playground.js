30c0c1096076580a01fc63fa17eae8bc65ed9ff2
Nicholas Zachary <nicholas.zachary@outlook.com>
2018-11-30 00:29:22 +0000

    answer

---
+++ closures-playground.js
@@ -0,0 +1,18 @@
+var foo = (function() {
+  var x = 10;
+
+  var inner = function() {
+    console.log("Value of x is: " + x);
+  }
+  return inner;
+}); // Notice the () at the end,
+      // which will immediately invoke the function,
+      // assigning the returned value (inner) to foo
+
+foo();
+// console.log(foo());
+
+
+var yes = foo()();
+var another = foo()();
+var andAnother = foo()();
